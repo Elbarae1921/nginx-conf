e012aca4e02ecbee16ebeb7f5c79e4816ec7b8e4
Elbarae Rguig <elbarae1921@gmail.com>
2021-11-26 23:29:22 +0100

    fix script

--- run-certbot.sh
+++ run-certbot.sh
@@ -3,5 +3,5 @@
 cat <<EOF | docker exec --interactive nginx sh
 apt-get update
 apt-get install certbot python3-certbot-nginx -y
-certbot run -n --nginx --agree-tos -d api.skeduler.elbarae.me,www.api.skeduler.elbarae.me  -m  elbarae1921@gmail.com  --redirect'
+certbot run -n --nginx --agree-tos -d api.skeduler.elbarae.me,www.api.skeduler.elbarae.me  -m  elbarae1921@gmail.com  --redirect
 EOF
