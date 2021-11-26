e034912e30eb4bbda978f6e1f7ee3a17168dafee
Elbarae Rguig <elbarae1921@gmail.com>
2021-11-26 23:21:40 +0100

    change script

--- run-certbot.sh
+++ run-certbot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker exec nginx "sudo apt-get update &&
-                    sudo apt-get install certbot python3-certbot-nginx -y &&
-                    certbot run -n --nginx --agree-tos -d api.skeduler.elbarae.me,www.api.skeduler.elbarae.me  -m  elbarae1921@gmail.com  --redirect"
+cat <<EOF | docker exec nginx sh
+apt-get update
+apt-get install certbot python3-certbot-nginx -y
+certbot run -n --nginx --agree-tos -d api.skeduler.elbarae.me,www.api.skeduler.elbarae.me  -m  elbarae1921@gmail.com  --redirect'
+EOF
