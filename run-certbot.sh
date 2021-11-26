391f0ab028aaf8bf24b1120af5358e0dfb89153b
Elbarae Rguig <elbarae1921@gmail.com>
2021-11-26 23:26:59 +0100

    add tty flag to docker exec

--- run-certbot.sh
+++ run-certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<EOF | docker exec nginx sh
+cat <<EOF | docker exec -t nginx sh
 apt-get update
 apt-get install certbot python3-certbot-nginx -y
 certbot run -n --nginx --agree-tos -d api.skeduler.elbarae.me,www.api.skeduler.elbarae.me  -m  elbarae1921@gmail.com  --redirect'
